ff23e10ac8d5ebfcd04f8b3ae313fb8a6e7cea9f
mexok <konstantin.coswig@gmail.com>
2023-04-05 19:11:02 +0200

    removed formatters; to be added as needed

--- .config/nvim/plugins.lua
+++ .config/nvim/plugins.lua
@@ -93,9 +93,6 @@ require("mason").setup()
 local null_ls = require("null-ls")
 null_ls.setup({
     sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.completion.spell,
     },
 })
 
