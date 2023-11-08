fd718334be69cda2b914c577be16fbf9f22e737a
Konstantin Merker <konstantin@semerker.de>
2023-11-08 16:35:33 +0100

    added Azure CLI to install.sh

--- install.sh
+++ install.sh
@@ -62,5 +62,20 @@ sudo apt install terraform
 touch ~/.zshrc
 terraform -install-autocomplete
 
+# Azure CLI
+sudo apt update
+sudo apt install ca-certificates curl apt-transport-https lsb-release gnupg
+curl -sLS https://packages.microsoft.com/keys/microsoft.asc |
+    gpg --dearmor |
+    sudo tee /etc/apt/keyrings/microsoft.gpg > /dev/null
+sudo chmod go+r /etc/apt/keyrings/microsoft.gpg
+
+AZ_DIST=$(lsb_release -cs)
+echo "deb [arch=`dpkg --print-architecture` signed-by=/etc/apt/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/azure-cli/ $AZ_DIST main" |
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+
+sudo apt update
+sudo apt install azure-cli
+
 # upgrade all packages
 sudo apt upgrade
