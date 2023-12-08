de2ac9900f3f401e20c83510f79a0185eff9373c
Konstantin Merker <konstantin@semerker.de>
2023-12-08 17:47:12 +0100

    refurbished setup.sh; also moved functionality from install.sh into setup.sh

--- README.md
+++ README.md
@@ -11,20 +11,29 @@ Goals of my setup
 =================
 
 * Have an environment optimized for doing programming
-* Don't move hand to mouse because misclicking is annoying and interrupts the workflow
-* Don't use pinky fingers too often as they are not so strong and hurt if used too often during long sessions
+* Don't move hand to mouse because misclicking is annoying and interrupts the
+  workflow
+* Don't use pinky fingers too often as they are not so strong and hurt if used
+  too often during long sessions
 
 To solve these goals, I've taken following steps:
 
 * Utilize a customized setup using nvim, tmux, zsh with oh-my-zsh
-* Have a Kinesis Advantage 360 keyboard with custom keybindings, so that e.g. Ctrl + Shift are reachable with ring and middle fingers as they are stronger then pinky.
+* Have a Kinesis Advantage 360 keyboard with custom keybindings, so that e.g.
+  Ctrl + Shift are reachable with ring and middle fingers as they are stronger
+  then pinky.
 
 
 Things to notice
 ================
 
-* I am from Germany and have adjusted the US-Layout coming with the 360 keyboard to fit in German special chars. I also applied mappings to them in NeoVIM, therefore I will have some keyboard bindings on keys like 'ö' or remappings of 'y' and 'z' because on my keyboard they are switched.
-* I was also inspired by the idea of Helix using a selection -> action pattern instead of vim's default action -> selection pattern. Therefore, I have bindings to jump right into selection, e.g. 'c' -> 'viw'
+* I am from Germany and have adjusted the US-Layout coming with the 360 keyboard
+  to fit in German special chars. I also applied mappings to them in NeoVIM,
+  therefore I will have some keyboard bindings on keys like 'ö' as I it is
+  convenient for me.
+* I was also inspired by the idea of Helix using a selection -> action pattern
+  instead of vim's default action -> selection pattern. Therefore, I have
+  bindings to jump right into selection, e.g. 'c' -> 'viw'
 
 
 How to setup
@@ -36,16 +45,15 @@ Please install:
 * zsh and oh-my-zsh
 * tmux
 * nvim (0.9 from official tar.gz package) and packer
-* I have included an install.sh which includes some of the above plus some more CLI-Tools I use
 
-Then copy the config files to their approriate locations:
+I have included an install.sh which includes some of the above plus some more
+CLI-Tools I use and also programming language specific setups. However, it
+overrides some of your local configs, e.g. for tmux and neovim, so you might
+want to read it first before executing them, so use with care.
 
-* .config/ and .tmux.conf -> just copy paste to home dir
-* .zshrc -> append to home dir .zshrc
+Following steps are not automated yet in the install.sh script:
+
+* Modifying .zshrc -> append everything from .zshrc to home dir .zshrc
 * Install nerdfont (I use DejaVu Sans Mono)
-
-You can also use the setup.sh script, but it overrides some of your local configs, e.g. for tmux and neovim, so use with care.
-
-Install programming specific language tools. I have not 'standardized' them for my setup yet.
 
 Feel free to reach out to me for problems/ issues.
--- install.sh
+++ install.sh
@@ -1,41 +1,60 @@
 #ssh-keygen -t ed25519 -C "konstantin@semerker.de"
 apt update
-apt install git
+
+# nvim
+sudo wget -o /opt/nvim-linux64.tar.gz https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+tar -zxf /opt/nvim-linux64.tar.gz
+ln -s /opt/nvim-linux64/bin/nvim /usr/local/bin/nvim
+
+mkdir -p "$HOME/.config/nvim"
+rm $HOME/.config/nvim/*.lua 2>/dev/null
+
+for f in ".config/nvim"/*.lua
+do
+    ln -s "$cwd/$f" "$HOME/$f"
+done
+
+# git
+sudo apt install -y git
 git config --global core.editor "nvim"
-apt install tmux
-apt install zsh
+
+# tmux and zsh
+sudo apt install -y tmux
+sudo apt install -y zsh
 chsh -s $(which zsh)
-apt install curl
+sudo apt install -y curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-apt install ripgrep
-apt install xsel
-apt install rename
+rm $HOME/.tmux.conf
+ln -s ./.tmux.conf "$HOME/.tmux.conf"
+
+# Other CLI
+sudo apt install -y ripgrep
+sudo apt install -y xsel
+sudo apt install -y rename
 
 # Nodejs
 curl -fsSL https://deb.nodesource.com/setup_20.x | bash - &&\
-apt install -y nodejs
+sudo apt install -y nodejs
 
 npm install -g typescript-language-server typescript
 npm install vls -g
 
 # go
-apt install -y golang
+sudo apt install -y golang
 go install github.com/go-delve/delve/cmd/dlv@latest
 
 # Python
-apt install -y python3-dev
-apt install -y python3-pip
-apt install -y python3-venv
-apt install -y python3-debugpy
-apt install -y python3-pytest
+sudo apt install -y python3-dev
+sudo apt install -y python3-pip
+sudo apt install -y python3-venv
+sudo apt install -y python3-debugpy
+sudo apt install -y python3-pytest
 
 # C/C++
-apt install -y clangd
-apt install -y llvm
-apt install -y llvm-lldb
+sudo apt install -y clangd
+sudo apt install -y llvm
+sudo apt install -y llvm-lldb
 
 # Docker
 # see: https://docs.docker.com/engine/install/debian/
@@ -43,7 +62,7 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 # for refresh - or relog
 newgrp docker
-apt install docker-compose-plugin
+sudo apt install -y docker-compose-plugin
 
 # Terraform
 sudo apt install gnupg software-properties-common
@@ -57,14 +76,14 @@ https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
 sudo tee /etc/apt/sources.list.d/hashicorp.list
 
 sudo apt update
-sudo apt install terraform
+sudo apt install -y terraform
 
 touch ~/.zshrc
 terraform -install-autocomplete
 
 # Azure CLI
 sudo apt update
-sudo apt install ca-certificates curl apt-transport-https lsb-release gnupg
+sudo apt install -y ca-certificates curl apt-transport-https lsb-release gnupg
 curl -sLS https://packages.microsoft.com/keys/microsoft.asc |
     gpg --dearmor |
     sudo tee /etc/apt/keyrings/microsoft.gpg > /dev/null
@@ -75,29 +94,31 @@ echo "deb [arch=`dpkg --print-architecture` signed-by=/etc/apt/keyrings/microsof
     sudo tee /etc/apt/sources.list.d/azure-cli.list
 
 sudo apt update
-sudo apt install azure-cli
+sudo apt install -y azure-cli
 
 # Kubernetes CLI
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl
+sudo apt install -y apt-transport-https ca-certificates curl
 
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt update
-sudo apt install kubectl
+sudo apt install -y kubectl
 
 # Install HELM
 curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
-sudo apt install apt-transport-https --yes
+sudo apt install -y apt-transport-https
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 sudo apt update
-sudo apt install helm
+sudo apt install -y helm
 
 # Install java for pyspark
 
-sudo apt install default-jdk
+sudo apt install -y default-jdk
 
 # upgrade all packages
 sudo apt upgrade
+
+echo "Please update .zshrc manually."
